f89d905afa2cfab428b381f6048cd43f3c8415b1
Franck Sharif <77142060+sharif2307@users.noreply.github.com>
2022-09-26 10:36:41 +0800

    Add files via upload

---
+++ Portfolio website 2.0/JS/main.js
@@ -0,0 +1,10 @@
+//RESPONSIVE NAVIGATION BAR
+const hamburger = document.querySelector(".menu-hamburger");
+const navbar = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("hide");
+});
+
+//PRELOADER
+// For Live Projects
